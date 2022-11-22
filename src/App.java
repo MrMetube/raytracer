ad43838f46894474580971c12c834f167ac48810
von Westarp, Viktor <viktor.grafvonwestarp@student.hs-anhalt.de>
2022-11-22 12:15:00 +0100

    changes

--- src/App.java
+++ src/App.java
@@ -1,8 +1,14 @@
 import shader.*;
+import geometry.*;
+import math.*;
 import scene.*;
 
 class App {
     public static void main(String[] args){
-        new Scene("./scenes/myScene.json").makeImage(new NormalShader(), "test");
+        new Scene("./scenes/myScene.json").makeImage(new NormalShader(),   "test1");
+        new Scene("./scenes/Example 1.json").makeImage(new NormalShader(), "test2");
+        new Scene("./scenes/Example 2.json").makeImage(new NormalShader(), "test3");
+        new Scene("./scenes/Example 3.json").makeImage(new NormalShader(), "test4");
+        new Scene("./scenes/Example 4.json").makeImage(new NormalShader(), "test5");
     }
 }
