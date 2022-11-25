3725f7ef62ac45231c2e3dd49fe1f25a95dcdef4
Viktor von Westarp <viktor.grafvonwestarp@student.hs-anhalt.de>
2022-11-25 18:53:17 +0100

    changess

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,9 @@ import scene.*;
 
 class Main {
     public static void main(String[] args){
-        Scene s = new Scene("./scenes/spheres.json");
+        // Scene s = new Scene("./scenes/spheres.json");
+        
+        //#region scene setup
         // // s.addMaterial("white",  new Material(new Color(1.0,1.0,1.0), 0.1, 0.6, 0.9, 100, false));
         // // s.addMaterial("yellow", new Material(new Color(0.9,0.9,0.5), 0.1, 0.9, 0.2, 5, true));
         // // s.addMaterial("lime",   new Material(new Color(0.7,0.8,0.2), 0.1, 1, 0, 1, false));
@@ -23,16 +25,23 @@ class Main {
         // // s.addGeometry(new Sphere(new Point(0, 1.4, 1), 0.75,"white"));
         // // s.addGeometry(new Sphere(new Point(1, 1.2, 1), 0.75,"yellow"));
         // // s.addGeometry(new Sphere(new Point(2, 1.0, 1), 0.75,"lime"));
-
+        
+        // s.toJson("example");
+        //#endregion
+        
+        //#region make Image
         // s.makeImage(new AmbientShader());
 
         // s.makeImage(new DiffuseShader());
 
         // s.makeImage(new SpecularShader());
+        //#endregion
+        
+        // new App();
+
+        //#region testing speed
         for (int i = 0; i < 10; i++)
             s.makeImage(new LightShader(),true);
-        // s.toJson("example");
-
-        // new App();
+        //#endregion
     }
 }
