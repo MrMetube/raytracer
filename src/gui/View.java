8a687aa5cd40ef5ba9b5c7ace3b65794bc8acfa9
Viktor von Westarp <viktor.grafvonwestarp@student.hs-anhalt.de>
2022-12-09 22:37:30 +0100

    Added FPS

--- src/gui/View.java
+++ src/gui/View.java
@@ -6,6 +6,10 @@ import javax.swing.JPanel;
 
 public class View extends JPanel {
     BufferedImage image;
+    long time;
+    int counter = 0;
+    int maxfps = 0;
+    int minfps = 2000;
 
     public View(int width, int height){
         super();
@@ -21,5 +25,18 @@ public class View extends JPanel {
     public void paint(Graphics g) {
         super.paint(g);
         if(image != null) g.drawImage(image, 0, 0, this);
+        
+        counter++;
+        double dif = System.nanoTime()-time;
+        long fps = Math.round(1_000_000_000/dif);
+        maxfps = Math.max((counter%50 == 0) ? 0 : maxfps, (int)fps);
+        minfps = Math.min((counter%50 == 0) ? 2000 : minfps, (int)fps);
+
+        g.drawString(String.format("%d FPS",fps), 5, 15);
+        g.drawString(String.format("%d minFPS",minfps), 5, 30);
+        g.drawString(String.format("%d maxFPS",maxfps), 5, 45);
+
+        
+        time = System.nanoTime();
     }
 }
--- src/raytracer/Trace.java
+++ src/raytracer/Trace.java
@@ -3,20 +3,18 @@ package raytracer;
 import math.Color;
 import raytracer.geometry.Geometry;
 
-import java.awt.image.BufferedImage;
-
 import shader.Shader;
 
 public class Trace implements Runnable{
-    final BufferedImage image;
+    final Color[][] buffer;
     final Color def = new Color(0.44, 0.85, 0.93);
     final Shader shader;
     final Scene scene;
     final Camera camera;
     final int start,end, totalHeight, totalWidth;
 
-    public Trace(Scene scene, int totalWidth, int totalHeight, Camera camera, BufferedImage image, int start, int deltaHeight, Shader shader){
-        this.image = image;
+    public Trace(int totalWidth, int totalHeight,  int start, int deltaHeight, Scene scene, Camera camera, Color[][] buffer,Shader shader){
+        this.buffer = buffer;
         this.scene = scene;
         this.shader = shader;
         this.camera = camera;
@@ -36,7 +34,7 @@ public class Trace implements Runnable{
                 color = color.add( ( payload.target() != null ) ? shader.getColor(payload, scene) : def);
             }
             color = color.div(payloads.length);
-            image.setRGB(u, totalHeight-v-1, color.rgb() );
+            buffer[u][totalHeight-v-1] = color;
         }
     }
 }
