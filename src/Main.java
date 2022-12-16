640a91c23610b03626f6c68e6bfcc91230cbe9fd
Viktor von Westarp <viktor.grafvonwestarp@student.hs-anhalt.de>
2022-12-16 16:45:58 +0100

    new scene

--- src/Main.java
+++ src/Main.java
@@ -1,39 +1,23 @@
-
 import gui.*;
-// import math.*;
-// import shader.*;
-// import raytracer.*;
-// import raytracer.light.*;
-// import raytracer.geometry.*;
+import math.*;
+import shader.*;
+import raytracer.*;
+import raytracer.light.*;
+import raytracer.geometry.*;
 
+@SuppressWarnings("unused")
 class Main {
     public static void main(String[] args){
         
-        // #region scene setup
-        // Scene s = new Scene();
+        // Scene s = Scene.randomSpheres(10);
         // s.addBasicMaterials();
         // s.addGeometry(new Plane(new Vector(0, 1, 0), new Point(0, -1, 0),5,"lime"));
 
-        // s.addLightSource(new DirectionalLight(new Vector(0.5, -1, 0.2), Color.WHITE, 0.5));
-        // s.addLightSource(new PointLight(new Point(0, 5, 0), Color.PURPLE, 5));
+        // s.addGeometry(new Sphere(new Point(1.001,0,0), 1, "silver"));
+        // s.addGeometry(new Sphere(new Point(-1.001,0,0), 1, "gold"));
 
-        // s.addGeometry(new Cube(new Point(1, 0.5, 1), new Point(2, 1.5, 2),"turquoise"));
-        // s.addGeometry(new Cube(new Point(1, 0, 0), new Point(2, 1, 2),"lemon"));
-        // s.addGeometry(new Cube(new Point(-1.1, -1, -1.1), new Point(0, 0.1, 0),"pink"));
-        // s.addGeometry(new Cube(new Point(0, -1, -1), new Point(1, 1, 1),"orange"));
-        
-        // s.toJson("cube");
-        //#endregion
+        // s.toJson("reflection");
         
         new App();
-
-        //#region testing speed
-        
-        // new App().timedRender(new Phong(), 10);
-        // new App().timedRender(new Phong(), 10);
-        // new App().timedRender(new BlinnPhong(), 10);
-        // new App().timedRender(new BlinnPhong(), 10);
-        
-        //#endregion
     }
 }
